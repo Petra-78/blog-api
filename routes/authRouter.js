62a2d09e719d8a6c3353dc81b1d581e5cb920e4e
Petra-78 <partos.petra.0305@gmail.com>
2026-01-06 20:30:57 +0100

    Import authController and use controller in post request

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 const router = Router();
 
+import { postLogin } from "../controllers/authController.js";
+
 router.post("/signup", (req, res) => {
   res.json({ message: "signup route" });
 });
 
-router.post("/login", (req, res) => {
-  res.json({ message: "login route" });
-});
+router.post("/login", postLogin);
 
 export { router as authRouter };
