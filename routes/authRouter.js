dcef0bf02d397a3921d2ef047a60c47d4318a0f2
Petra-78 <partos.petra.0305@gmail.com>
2026-01-07 16:54:58 +0100

    Add admin login route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,12 +2,18 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 const router = Router();
 
-import { postLogin, postSignup } from "../controllers/authController.js";
+import {
+  postLogin,
+  postSignup,
+  postAdminLogin,
+} from "../controllers/authController.js";
 import verifyClient from "../auth/authMiddleware.js";
+import { verifyAdmin } from "../auth/authMiddleware.js";
 
 router.post("/signup", postSignup);
 
 router.post("/login", postLogin);
+router.post("/admin", postAdminLogin);
 router.get("/protected", verifyClient, (req, res) => {
   res.json({ message: "you are on protected route", user: req.user });
 });
