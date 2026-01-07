e0a174698f4d2412a2dda9149c84ba4b58d34c7b
Petra-78 <partos.petra.0305@gmail.com>
2026-01-07 22:17:22 +0100

    Add comments router

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -10,11 +10,15 @@ import {
   deletePost,
 } from "../controllers/postsController.js";
 
+import { commentsRouter } from "./commentsRouter.js";
+
 router.get("/", getAllPosts);
 router.get("/:id", getPost);
 
 router.post("/", verifyClient, verifyAdmin, createPost);
 router.put("/:id", verifyClient, verifyAdmin, updatePost);
 router.delete("/:id", verifyClient, verifyAdmin, deletePost);
+
+router.use("/:postId/comments", commentsRouter);
 
 export { router as postsRouter };
