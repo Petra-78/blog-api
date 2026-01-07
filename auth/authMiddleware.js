e7d93cb50ad5b9ac5d773ff4abe7731fd39cb31a
Petra-78 <partos.petra.0305@gmail.com>
2026-01-07 16:54:14 +0100

    Add admin authorization

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -17,3 +17,10 @@ export default function verifyClient(req, res, next) {
     return res.status(401).json({ message: "Invalid token" });
   }
 }
+
+export function verifyAdmin(req, res, next) {
+  if (!req.user.isAdmin) {
+    return res.status(403).json({ message: "Admin only" });
+  }
+  next();
+}
