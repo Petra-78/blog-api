3d8d2ef75efbda5de8afe1217187a470658c06ff
Petra-78 <partos.petra.0305@gmail.com>
2026-01-07 22:48:13 +0100

    Delete test route + remove unused imports

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,14 +6,9 @@ import {
   postSignup,
   postAdminLogin,
 } from "../controllers/authController.js";
-import { verifyClient, verifyAdmin } from "../auth/authMiddleware.js";
 
 router.post("/signup", postSignup);
-
 router.post("/login", postLogin);
 router.post("/admin", postAdminLogin);
-router.get("/protected", verifyClient, (req, res) => {
-  res.json({ message: "you are on protected route", user: req.user });
-});
 
 export { router as authRouter };
